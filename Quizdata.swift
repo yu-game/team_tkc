de7db54b2d9dc4998861d310c7405dddc6bc418d
yu mikami <y.mikami@yunoMacBook-Pro.local>
2020-09-17 12:25:18 +0900

    data file v1

--- Quizdata.swift
+++ Quizdata.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+
+class Quizdata{
+    /*
+    let Quizsentences : String
+    let Choice_A : String
+    let Choice_B : String
+    let Choice_C : String
+    let Choice_D : String
+    let Choice_ans : String
+    let Commentary : String
+    */
+    let qsent : String
+    let cA : String
+    let cB : String
+    let cC : String
+    let cD : String
+    let cans : String
+    let comment : String
+    
+    
+    init(dic : [String:Any]){
+        self.qsent = dic["qsent"] as? String ?? ""
+        self.cA = dic["cA"] as? String ?? ""
+        self.cB = dic["cB"] as? String ?? ""
+        self.cC = dic["cC"] as? String ?? ""
+        self.cD = dic["cD"] as? String ?? ""
+        self.cans = dic["cans"] as? String ?? ""
+        self.comment = dic["comment"] as? String ?? ""
+    }
+}
