0261c8c5d61efee7cc5151b5f4076bd614bc099e
yu mikami <y.mikami@yunoMacBook-Pro.local>
2020-09-17 12:24:07 +0900

    swift file v2

--- QuizanswerViewController.swift
+++ QuizanswerViewController.swift
@@ -6,4 +6,74 @@
 //  Copyright © 2020 yu.m. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Firebase
+import FirebaseFirestore
+import FirebaseAuth
+
+class QuizanswerViewController: UIViewController {
+    
+    private var quizdata = [Quizdata]()
+    
+    @IBOutlet weak var choice_a: UIButton!
+    @IBOutlet weak var choice_b: UIButton!
+    @IBOutlet weak var choice_c: UIButton!
+    @IBOutlet weak var choice_d: UIButton!
+    
+    
+    
+    //quiztableview
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fetchQuizFromFirestore() 
+        
+    }
+    
+    private func fetchQuizFromFirestore(){
+        Firestore.firestore().collection("quiz").getDocuments { (snapshots, err) in
+            if let err = err {
+                print("Firestoreへのアクセスに失敗しました。\(err)")
+                return
+            }
+            snapshots?.documents.forEach({ (snapshot) in
+                let dic = snapshot.data()
+                let qdata = Quizdata.init(dic: dic)
+                
+                self.quizdata.append(qdata)
+                
+                self.quizdata.forEach { (qdata) in
+                    print("test data:", qdata.qsent)
+                }
+                
+                print("data:",qdata)
+            })
+        }
+    }
+}
+
+class QuizanswerViewCell: UITableViewCell{
+    
+    var qdata: Quizdata?{
+        didSet{
+
+        }
+    }
+    
+    @IBAction func qsentence(_ sender: Any) {
+    
+    }
+    @IBAction func choice_a(_ sender: UIButton) {
+        sender.setTitle(qdata?.cA, for: .normal)
+    }
+    
+    @IBAction func choice_b(_ sender: Any) {
+    }
+    
+    @IBAction func choice_c(_ sender: Any) {
+    }
+    @IBAction func choice_d(_ sender: Any) {
+    }
+    
+}
